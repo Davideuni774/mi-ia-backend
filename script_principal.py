3d5b12029aeae28befd335f1357cf3bc4a6f897a
Davideuni774 <est.davide.beltran@unimilitar.edu.co>
2026-04-13 03:30:53 -0500

    Update script_principal.py

--- script_principal.py
+++ script_principal.py
@@ -17,7 +17,7 @@ load_dotenv()
 class AppConfig:
     POPPLER_PATH = os.getenv("POPPLER_PATH", None)
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEYS")
-    MODEL_NAME = os.getenv("MODEL", "gemini-1.0-pro-vision")
+    MODEL_NAME = os.getenv("MODEL", "gemini-2.0-flash")
     MAX_PDF_PAGES = 8
     IMAGE_SIZE = (1200, 1200)
 
